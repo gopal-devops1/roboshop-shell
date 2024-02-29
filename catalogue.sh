a4e6631db9454fb7c0524fe4828f65822f95a27f
gopal-devops1 <gopaldevops97@gmail.com>
2024-02-29 20:28:49 +0530

    roboshop shell scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-MongoDB_HOST=mongodb.devops1.website
+MONGDB_HOST=mongodb.devops1.website
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
@@ -30,7 +30,7 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-dnf module diable nodejs -y
+dnf module disable nodejs -y &>> $LOGFILE
 
 VALIDATE $? "Disabling current NodeJS"
 
